b200caa343e3f82a57bf3b1aa3d93d72095eea97
Caleb Jones <calebhannonjones@gmail.com>
2018-02-10 00:06:37 -0500

    Layout + Portfolio Updates

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 $(document).foundation();
-
+$(".meter > span").each(function() {
+  $(this)
+    .data("origWidth", $(this).width())
+    .width(0)
+    .animate({
+      width: $(this).data("origWidth") // or + "%" if fluid
+    }, 1200);
+});
